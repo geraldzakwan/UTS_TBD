3e1753d97405c0030ab41028500e69efae312bc9
varian97 <variancaesar@gmail.com>
2017-11-13 00:33:58 +0700

    save and load graph

--- centrality.py
+++ centrality.py
@@ -1,5 +1,6 @@
 import snap
 import sys
+import os
 
 def extract_domain(line):
 	domain_raw = line.split('\t')[1]
@@ -8,44 +9,74 @@ def extract_domain(line):
 	domain_net = domain_net.replace('\n', '')
 	return domain_net
 
-filename = '../' + sys.argv[1]
+
 domain_mapping = []
-counter = int(sys.argv[2])
-last_domain = -1
-testing_line = ''
-
 g1 = snap.TNGraph.New()
-with open(filename) as infile:
-	for line in infile:
-		if('P' in line):
-			testing_line += line
-			counter -= 1
-			domain_name = extract_domain(line)
-			nodeid = -1
-			if domain_name in domain_mapping:
-				nodeid = domain_mapping.index(domain_name)
-			else:
-				domain_mapping.append(domain_name)
-				nodeid = len(domain_mapping) - 1
-				g1.AddNode(nodeid)
-			last_domain = nodeid
 
-		elif('L' in line):
-			testing_line += line
-			domain_name = extract_domain(line)
-			node_link_id = -1
-			if domain_name in domain_mapping:
-				node_link_id = domain_mapping.index(domain_name)
-			else:
-				domain_mapping.append(domain_name)
-				node_link_id = len(domain_mapping) - 1
-				g1.AddNode(node_link_id)
+files_in_directory = os.listdir('./')
+if("memetracker1.graph" in files_in_directory and "domain_mapping.txt" in files_in_directory):
+	load_choice = raw_input("Graph data found, want to load it instead(Y/N)? ")
+	if(load_choice.lower() == 'y'):
+		# load graph model
+		FIn = snap.TFIn("memetracker1.graph")
+		g1 = snap.TNGraph.Load(FIn)
 
-			print "add edge from %d to %d" % (last_domain, node_link_id)
-			g1.AddEdge(last_domain, node_link_id)
+		# load mapping
+		with open("domain_mapping.txt") as infile:
+			for line in infile:
+				if(len(line) > 1):
+					domain_mapping.append(line.replace('\n', ''))
+	else :
+		filename = '../' + sys.argv[1]
+		counter = int(sys.argv[2])
+		last_domain = -1
 
-		if (counter == 0):
-			break
+		with open(filename) as infile:
+			for line in infile:
+				if('P' in line):
+					counter -= 1
+					domain_name = extract_domain(line)
+					nodeid = -1
+					if domain_name in domain_mapping:
+						nodeid = domain_mapping.index(domain_name)
+					else:
+						domain_mapping.append(domain_name)
+						nodeid = len(domain_mapping) - 1
+						g1.AddNode(nodeid)
+					last_domain = nodeid
+
+				elif('L' in line):
+					domain_name = extract_domain(line)
+					node_link_id = -1
+					if domain_name in domain_mapping:
+						node_link_id = domain_mapping.index(domain_name)
+					else:
+						domain_mapping.append(domain_name)
+						node_link_id = len(domain_mapping) - 1
+						g1.AddNode(node_link_id)
+
+					print "add edge from %d to %d" % (last_domain, node_link_id)
+					g1.AddEdge(last_domain, node_link_id)
+
+				if (counter == 0):
+					break
+
+		print "Graph built successfully !"
+		choice = raw_input("Save the graph for future use (Y/N)? ")
+		if choice.lower() == 'y':
+			# save the graph model
+			FOut = snap.TFOut("memetracker1.graph")
+			g1.Save(FOut)
+			FOut.Flush()
+
+			# save the domain mapping
+			fout = open('domain_mapping.txt', 'w')
+			for item in domain_mapping:
+				fout.write(item + '\n')
+			fout.close()
+
+			print "Graph model and mapping has been saved !"
+
 
 print "\n======================================"
 print "Node Mapping : "
