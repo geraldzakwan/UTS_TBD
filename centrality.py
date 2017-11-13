ed203cd31b90d1c518499b7d8bd0f52f2862d89a
varian97 <variancaesar@gmail.com>
2017-11-13 23:27:14 +0700

    degree centrality

--- centrality.py
+++ centrality.py
@@ -1,22 +1,28 @@
+from __future__ import division
 import snap
 import sys
 import os
+import Queue as Q
+import csv
 
 def extract_domain(line):
 	domain_raw = line.split('\t')[1]
 	temp = domain_raw.split('/')
-	domain_net = temp[0] + temp[1] + temp[2]
+	domain_net = temp[2]
 	domain_net = domain_net.replace('\n', '')
 	return domain_net
 
 
 domain_mapping = []
 g1 = snap.TNGraph.New()
+isload = None
 
+# want to load ?
 files_in_directory = os.listdir('./')
 if("memetracker1.graph" in files_in_directory and "domain_mapping.txt" in files_in_directory):
 	load_choice = raw_input("Graph data found, want to load it instead(Y/N)? ")
 	if(load_choice.lower() == 'y'):
+		isload = True
 		# load graph model
 		FIn = snap.TFIn("memetracker1.graph")
 		g1 = snap.TNGraph.Load(FIn)
@@ -27,62 +33,75 @@ if("memetracker1.graph" in files_in_directory and "domain_mapping.txt" in files_
 				if(len(line) > 1):
 					domain_mapping.append(line.replace('\n', ''))
 	else :
-		filename = '../' + sys.argv[1]
-		counter = int(sys.argv[2])
-		last_domain = -1
+		isload = False
+else:
+	isload = False
 
-		with open(filename) as infile:
-			for line in infile:
-				if('P' in line):
-					counter -= 1
-					domain_name = extract_domain(line)
-					nodeid = -1
-					if domain_name in domain_mapping:
-						nodeid = domain_mapping.index(domain_name)
-					else:
-						domain_mapping.append(domain_name)
-						nodeid = len(domain_mapping) - 1
-						g1.AddNode(nodeid)
-					last_domain = nodeid
+# make graph from source
+if(not isload):
+	filename = '../' + sys.argv[1]
+	counter = int(sys.argv[2])
+	last_domain = -1
 
-				elif('L' in line):
-					domain_name = extract_domain(line)
-					node_link_id = -1
-					if domain_name in domain_mapping:
-						node_link_id = domain_mapping.index(domain_name)
-					else:
-						domain_mapping.append(domain_name)
-						node_link_id = len(domain_mapping) - 1
-						g1.AddNode(node_link_id)
+	with open(filename) as infile:
+		for line in infile:
+			if('P' in line):
+				counter -= 1
+				domain_name = extract_domain(line)
+				nodeid = -1
+				if domain_name in domain_mapping:
+					nodeid = domain_mapping.index(domain_name)
+				else:
+					domain_mapping.append(domain_name)
+					nodeid = len(domain_mapping) - 1
+					g1.AddNode(nodeid)
+				last_domain = nodeid
 
-					print "add edge from %d to %d" % (last_domain, node_link_id)
-					g1.AddEdge(last_domain, node_link_id)
+			elif('L' in line):
+				domain_name = extract_domain(line)
+				node_link_id = -1
+				if domain_name in domain_mapping:
+					node_link_id = domain_mapping.index(domain_name)
+				else:
+					domain_mapping.append(domain_name)
+					node_link_id = len(domain_mapping) - 1
+					g1.AddNode(node_link_id)
 
-				if (counter == 0):
-					break
+				print "add edge from %d to %d" % (last_domain, node_link_id)
+				g1.AddEdge(last_domain, node_link_id)
 
-		print "Graph built successfully !"
-		choice = raw_input("Save the graph for future use (Y/N)? ")
-		if choice.lower() == 'y':
-			# save the graph model
-			FOut = snap.TFOut("memetracker1.graph")
-			g1.Save(FOut)
-			FOut.Flush()
+			if (counter == 0):
+				break
 
-			# save the domain mapping
-			fout = open('domain_mapping.txt', 'w')
-			for item in domain_mapping:
-				fout.write(item + '\n')
-			fout.close()
+	print "Graph built successfully (%d nodes, %d edges)!" % (g1.GetNodes(), g1.GetEdges())
+	choice = raw_input("Save the graph for future use (Y/N)? ")
+	if choice.lower() == 'y':
+		# save the graph model
+		FOut = snap.TFOut("memetracker1.graph")
+		g1.Save(FOut)
+		FOut.Flush()
 
-			print "Graph model and mapping has been saved !"
+		# save the domain mapping
+		fout = open('domain_mapping.txt', 'w')
+		for item in domain_mapping:
+			fout.write(item + '\n')
+		fout.close()
 
-print "\n======================================"
-print "Betweenness Centrality : "
-print "======================================\n"
-Nodes = snap.TIntFltH()
-Edges = snap.TIntPrFltH()
-snap.GetBetweennessCentr(g1, Nodes, Edges, 1.0)
-print "%-50s %s" % ("Domain Name", "Betweenness Centrality\n")
-for node, domain in zip(Nodes, domain_mapping):
-	print "%-50s %s" % (domain_mapping[node], Nodes[node])
+		print "Graph model and mapping has been saved !"
+
+if not os.path.exists('.\\centrality_result'):
+	os.makedirs('.\\centrality_result')
+
+# calculating node centrality and save it as csv
+with open('.\\centrality_result\\deg_centrality.csv', 'wb') as csvfile:
+	q = Q.PriorityQueue()
+	for NI in g1.Nodes():
+		q.put((-NI.GetInDeg(), domain_mapping[NI.GetId()]))
+	
+	writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	while not q.empty():
+		tup = q.get()
+		newtup = tup[0] * -1 * 0.01, tup[1]
+		writer.writerow(newtup)
+
+print "node centrality saved successfully !"
