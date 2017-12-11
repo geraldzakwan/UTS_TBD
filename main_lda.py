8faaa6d5a4e11903aea8a790924bc63006db5dc5
geraldzakwan <geraldi_dzakwan@yahoo.co.id>
2017-12-11 09:40:13 +0800

    lda from example

---
+++ main_lda.py
@@ -0,0 +1,60 @@
+# PREPARING DOCUMENTS
+
+doc1 = "Sugar is bad to consume. My sister likes to have sugar, but not my father."
+doc2 = "My father spends a lot of time driving my sister around to dance practice."
+doc3 = "Doctors suggest that driving may cause increased stress and blood pressure."
+doc4 = "Sometimes I feel pressure to perform well at school, but my father never seems to drive my sister to do better."
+doc5 = "Health experts say that Sugar is not good for your lifestyle."
+
+# compile documents
+doc_complete = [doc1, doc2, doc3, doc4, doc5]
+
+# CLEANING AND PREPROCESSING
+
+# import nltk
+# nltk.download()
+
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+stop = set(stopwords.words('english'))
+exclude = set(string.punctuation)
+lemma = WordNetLemmatizer()
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    return normalized
+
+doc_clean = [clean(doc).split() for doc in doc_complete]
+
+# PREPARING DOCUMENT-TERM MATRIX
+
+# Ignore warning from Gensim
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+
+# Importing Gensim
+import gensim
+from gensim import corpora
+
+# Creating the term dictionary of our courpus, where every unique term is assigned an index.
+dictionary = corpora.Dictionary(doc_clean)
+
+# Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
+doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
+
+# RUNNING LDA MODEL
+
+# Creating the object for LDA model using gensim library
+Lda = gensim.models.ldamodel.LdaModel
+
+# Running and Trainign LDA model on the document term matrix.
+ldamodel = Lda(doc_term_matrix, num_topics=3, id2word = dictionary, passes=50)
+
+# RESULTS
+
+print(ldamodel.print_topics(num_topics=3, num_words=6))
+# ['0.168*health + 0.083*sugar + 0.072*bad,
+# '0.061*consume + 0.050*drive + 0.050*sister,
+# '0.049*pressur + 0.049*father + 0.049*sister]
